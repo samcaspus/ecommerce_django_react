bc6dad187637e6d15a3f2f5ee077009998321160
sandeep guptan <sandeepguptan@rediffmail.com>
2020-10-03 20:11:31 +0530

    created a custom login model

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50,default='None')
+    email = models.EmailField(max_length=200,unique=True)
+    # password = models.PasswordField(max_length=200,required=True)
+
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = []    
+
+    phone = models.CharField(max_length=20,blank=True,null=True)
+    gender = models.CharField(max_length=20,blank=True,null=True)
+
+    session_token = models.CharField(max_length=10,default=0)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+    
+
