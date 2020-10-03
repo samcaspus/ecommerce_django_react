f33e8fd0638e656eb0f38750adef4f859b74bf80
sandeep guptan <sandeepguptan@rediffmail.com>
2020-10-03 19:50:21 +0530

    added api for products

--- api/product/urls.py
+++ api/product/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from rest_framework import router
+from rest_framework import routers
+from .views import ProductViewSet
 
+router = routers.DefaultRouter()
+router.register(r'', ProductViewSet)
 
-urlpatterns = [
-    
+urlpatterns =[
+    path('',include(router.urls))
 ]
--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path('',home,name='api.home'),
     path('category/',include('api.category.urls')),
+    path('product/',include('api.product.urls')),
     
 
 ]
